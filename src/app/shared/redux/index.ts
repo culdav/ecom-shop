023d37edc3f36499ecb64c7a7a4a813b78860947
culdav <culdavlad93@gmail.com>
2022-11-20 11:42:47 +0200

    Removed unused code

--- src/app/shared/redux/index.ts
+++ src/app/shared/redux/index.ts
@@ -3,9 +3,4 @@ import { ProductState } from './product/index';
 import { CheckoutState } from './checkout/index';
 import { AppState } from './app.state';
 
-// export * from './user/index';
-// export * from './product/index';
-// export * from './checkout/index';
-// export * from './app.state';
-
 export const shopState = [UserState, ProductState, CheckoutState, AppState];
